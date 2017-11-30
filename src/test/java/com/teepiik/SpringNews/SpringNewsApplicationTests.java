95c235a67c8acc127f27dd37f10e9b2cb66745fc
teepiik <teemu.piik@gmail.com>
2017-11-30 13:25:18 +0200

    confs

--- src/test/java/com/teepiik/SpringNews/SpringNewsApplicationTests.java
+++ src/test/java/com/teepiik/SpringNews/SpringNewsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.teepiik.SpringNews;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,10 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringNewsApplicationTests {
-/*
+
 	@Test
 	public void contextLoads() {
+            Assert.assertTrue(true);
 	}
-*/
 
 }
