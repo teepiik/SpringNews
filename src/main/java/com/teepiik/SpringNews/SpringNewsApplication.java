319b7b466738be3021836d92e1124c39b5e7007c
teepiik <teemu.piik@gmail.com>
2017-11-30 11:31:00 +0200

    add all

---
+++ src/main/java/com/teepiik/SpringNews/SpringNewsApplication.java
@@ -0,0 +1,12 @@
+package com.teepiik.SpringNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewsApplication.class, args);
+	}
+}
